95c6e66debd0f787331261480bb50d9221b37631
Owain Lewis <owain.lewis@oracle.com>
2021-02-28 20:41:45 +0000

    Working reverse proxy

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,105 @@
-use std::{convert::Infallible, net::SocketAddr};
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Client, Method, Request, Response, Server};
+use hyper::{Body, Client, Request, Response, Server};
+use hyper_tls::HttpsConnector;
+use std::net::SocketAddr;
 
-type HttpClient = Client<hyper::client::HttpConnector>;
+/// HTTP headers to strip
+const STRIPPED: [&str; 6] = [
+    "content-length",
+    "transfer-encoding",
+    "accept-encoding",
+    "content-encoding",
+    "host",
+    "connection",
+];
 
-#[tokio::main]
-async fn main() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let client = HttpClient::new();
+#[derive(Debug)]
+enum ReverseProxyError {
+    Hyper(hyper::Error),
+    HyperHttp(hyper::http::Error),
+}
 
-    let make_service = make_service_fn(move |_| {
-        let client = client.clone();
-        async move { Ok::<_, Infallible>(service_fn(move |req| proxy(client.clone(), req))) }
-    });
-
-    let server = Server::bind(&addr).serve(make_service);
-
-    println!("Listening on http://{}", addr);
-
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
+impl From<hyper::Error> for ReverseProxyError {
+    fn from(e: hyper::Error) -> Self {
+        ReverseProxyError::Hyper(e)
     }
 }
 
-async fn proxy(client: HttpClient, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    println!("req: {:?}", req);
-    client.request(req).await
+impl From<hyper::http::Error> for ReverseProxyError {
+    fn from(e: hyper::http::Error) -> Self {
+        ReverseProxyError::HyperHttp(e)
+    }
+}
+
+impl std::fmt::Display for ReverseProxyError {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(fmt, "{:?}", self)
+    }
+}
+
+impl std::error::Error for ReverseProxyError {}
+
+struct ReverseProxy {
+    client: Client<HttpsConnector<hyper::client::HttpConnector>>,
+}
+
+impl ReverseProxy {    
+
+    async fn handle(&self, mut req: Request<Body>) -> Result<Response<Body>, ReverseProxyError> {
+        let h = req.headers_mut();
+        for key in &STRIPPED {
+            h.remove(*key);
+        }
+
+        let mut builder = hyper::Uri::builder()
+            .scheme("https")
+            .authority("httpbin.org");
+             
+        // Todo request modifications like copying headers etc 
+        if let Some(pq) = req.uri().path_and_query() {
+            builder = builder.path_and_query(pq.clone());
+        }
+
+        *req.uri_mut() = builder.build()?;
+
+        log::info!("request == {:?}", req);
+
+        let response = self.client.request(req).await?;
+        log::debug!("response == {:?}", response);
+        Ok(response)
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+
+    // Construct our HTTP client
+    let https = HttpsConnector::new();
+    let client = Client::builder().build(https);
+
+    let proxy_config = std::sync::Arc::new(ReverseProxy {
+        client
+    });
+
+    let make_svc = make_service_fn(move |_conn| {
+        let rp = proxy_config.clone();
+        async {
+            Ok::<_, ReverseProxyError>(service_fn(move |req| {
+                let rp = rp.clone();
+                async move { rp.handle(req).await }
+            }))
+        }
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+    
+    log::info!("Server started on {}", addr);
+
+    if let Err(e) = server.await {
+        log::error!("server error: {}", e);
+        std::process::abort();
+    }
 }
